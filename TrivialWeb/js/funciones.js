2a452245652905c6a6c974b7b7e829b5cfd20ff5
adrianiri <adrian.irigoyen.iraundegui@gmail.com>
2022-04-16 23:34:54 +0200

    Más pruebas...

--- TrivialWeb/js/funciones.js
+++ TrivialWeb/js/funciones.js
@@ -6,42 +6,66 @@ function getParentFolder() {
     return folderLevels[folderLevels.length-2]; //Para cuando cambio el nombre de Trivial para actualizar la caché
 }
 function visibilidadUno() {
-  var p1 = document.querySelector("#p1");
-  var p2 = document.querySelector("#p2");
-  var p3 = document.querySelector("#p3");
-  var p4 = document.querySelector("#p4");
-  p1.classList.remove("oculto");
-  p2.classList.add("oculto");
-  p3.classList.add("oculto");
-  p4.classList.add("oculto");
+    var p1 = document.querySelector("#p1");
+    var p2 = document.querySelector("#p2");
+    var p3 = document.querySelector("#p3");
+    var p4 = document.querySelector("#p4");
+    p1.classList.remove("oculto");
+    p2.classList.remove("oculto");
+    p3.classList.remove("oculto");
+    p4.classList.remove("oculto");
+    var crono = setTimeout(function() {
+        p1.classList.remove("oculto");
+        p2.classList.add("oculto");
+        p3.classList.add("oculto");
+        p4.classList.add("oculto");
+    }, 1000);
 }
 function visibilidadDos() {
     var p1 = document.querySelector("#p1");
     var p2 = document.querySelector("#p2");
     var p3 = document.querySelector("#p3");
     var p4 = document.querySelector("#p4");
-    p1.classList.add("oculto");
+    p1.classList.remove("oculto");
     p2.classList.remove("oculto");
-    p3.classList.add("oculto");
-    p4.classList.add("oculto");
+    p3.classList.remove("oculto");
+    p4.classList.remove("oculto");
+    var crono = setTimeout(function() {
+        p1.classList.add("oculto");
+        p2.classList.remove("oculto");
+        p3.classList.add("oculto");
+        p4.classList.add("oculto");
+    }, 1000);
   }
   function visibilidadTres() {
     var p1 = document.querySelector("#p1");
     var p2 = document.querySelector("#p2");
     var p3 = document.querySelector("#p3");
     var p4 = document.querySelector("#p4");
-    p1.classList.add("oculto");
-    p2.classList.add("oculto");
+    p1.classList.remove("oculto");
+    p2.classList.remove("oculto");
     p3.classList.remove("oculto");
-    p4.classList.add("oculto");
+    p4.classList.remove("oculto");
+    var crono = setTimeout(function() {
+        p1.classList.add("oculto");
+        p2.classList.add("oculto");
+        p3.classList.remove("oculto");
+        p4.classList.add("oculto");
+    }, 1000);
   }
   function visibilidadCuatro() {
     var p1 = document.querySelector("#p1");
     var p2 = document.querySelector("#p2");
     var p3 = document.querySelector("#p3");
     var p4 = document.querySelector("#p4");
-    p1.classList.add("oculto");
-    p2.classList.add("oculto");
-    p3.classList.add("oculto");
+    p1.classList.remove("oculto");
+    p2.classList.remove("oculto");
+    p3.classList.remove("oculto");
     p4.classList.remove("oculto");
+    var crono = setTimeout(function() {
+        p1.classList.add("oculto");
+        p2.classList.add("oculto");
+        p3.classList.add("oculto");
+        p4.classList.remove("oculto");
+    }, 1000);
   }
