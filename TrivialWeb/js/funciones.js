50ac49d8570c40284597e23ed212e9de3e5b0d68
adrianiri <adrian.irigoyen.iraundegui@gmail.com>
2022-04-16 23:24:07 +0200

    He dado invisibilidad al resto de pantallas, pero parece que ahora pierde el efecto de transición...

--- TrivialWeb/js/funciones.js
+++ TrivialWeb/js/funciones.js
@@ -5,3 +5,43 @@ function getParentFolder() {
     var folderLevels = currentUrl.baseURI.split("/");
     return folderLevels[folderLevels.length-2]; //Para cuando cambio el nombre de Trivial para actualizar la caché
 }
+function visibilidadUno() {
+  var p1 = document.querySelector("#p1");
+  var p2 = document.querySelector("#p2");
+  var p3 = document.querySelector("#p3");
+  var p4 = document.querySelector("#p4");
+  p1.classList.remove("oculto");
+  p2.classList.add("oculto");
+  p3.classList.add("oculto");
+  p4.classList.add("oculto");
+}
+function visibilidadDos() {
+    var p1 = document.querySelector("#p1");
+    var p2 = document.querySelector("#p2");
+    var p3 = document.querySelector("#p3");
+    var p4 = document.querySelector("#p4");
+    p1.classList.add("oculto");
+    p2.classList.remove("oculto");
+    p3.classList.add("oculto");
+    p4.classList.add("oculto");
+  }
+  function visibilidadTres() {
+    var p1 = document.querySelector("#p1");
+    var p2 = document.querySelector("#p2");
+    var p3 = document.querySelector("#p3");
+    var p4 = document.querySelector("#p4");
+    p1.classList.add("oculto");
+    p2.classList.add("oculto");
+    p3.classList.remove("oculto");
+    p4.classList.add("oculto");
+  }
+  function visibilidadCuatro() {
+    var p1 = document.querySelector("#p1");
+    var p2 = document.querySelector("#p2");
+    var p3 = document.querySelector("#p3");
+    var p4 = document.querySelector("#p4");
+    p1.classList.add("oculto");
+    p2.classList.add("oculto");
+    p3.classList.add("oculto");
+    p4.classList.remove("oculto");
+  }
