71e5f219d6baa03669facdbb2140b28b954de841
adrianiri <adrian.irigoyen.iraundegui@gmail.com>
2022-04-23 16:39:21 +0200

    Prueba de formato de pantalla PREGUNTAS

--- TrivialWeb/js/cuatro.js
+++ TrivialWeb/js/cuatro.js
@@ -2,22 +2,25 @@ function inicializarNivel() {
     var pregunta = document.querySelector("#pregunta");
     var encabezado = document.querySelector("#encabezado");
     var div_respuestas = document.querySelector("#div_respuestas");
-    pregunta.textContent = "Pregunta";
-    encabezado.textContent = "Encabezado...";
+    pregunta.innerHTML = `Pregunta x de y:<br><br><span style="font-weight:bold;">Pregunta?</span>`;
+    encabezado.innerHTML = `<span style="font-style:italic;">Encabezado...</span>`;
     div_respuestas.innerHTML = `<label class"anchisimo">
-                                    <input type="radio" name="respuestas" id="respuesta0" class"" value="">
+                                    <input type="radio" name="respuestas" id="respuesta0" class"" value="respuesta0">
                                     <label for="respuesta0"> Respuesta 0</label><br>
                                 </label>
+                                <br>
                                 <label class"anchisimo">
-                                    <input type="radio" name="respuestas" id="respuesta1" value="">
+                                    <input type="radio" name="respuestas" id="respuesta1" value="respuesta1">
                                     <label for="respuesta1"> Respuesta 1</label><br>
                                 </label>
+                                <br>
                                 <label class"anchisimo">
-                                    <input type="radio" name="respuestas" id="respuesta2" value="">
+                                    <input type="radio" name="respuestas" id="respuesta2" value="respuesta2">
                                     <label for="respuesta2"> Respuesta 2</label><br>
                                 </label>
+                                <br>
                                 <label class"anchisimo">
-                                    <input type="radio" name="respuestas" id="respuesta3" value="">
+                                    <input type="radio" name="respuestas" id="respuesta3" value="respuesta3">
                                     <label for="respuesta3"> Respuesta 3</label><br>
                                 </label>
     `
@@ -25,10 +28,10 @@ function inicializarNivel() {
 function extraerNivel() {
     var colecRespuestas = [];
     ///////////////////////// pregunta.textContent /////////////////////////
-    pregunta.innerHTML = `Pregunta ${numPregunta} de ${numPreguntas}:<br><br>${colecNiveles[0].getPregunta()}?`;
+    pregunta.innerHTML = `Pregunta ${numPregunta} de ${numPreguntas}:<br><br><span style="font-weight:bold;">${colecNiveles[0].getPregunta()}?</span>`;
     /////////////////////////////////////////////////////////////////////////
     ///////////////////////// encabezado.textContent ////////////////////////
-    encabezado.textContent = `${colecNiveles[0].getEncabezado()}`;
+    encabezado.innerHTML = `<span style="font-style:italic;">${colecNiveles[0].getEncabezado()}</span>`;
     /////////////////////////////////////////////////////////////////////////
     ///////////////////////// respuestas.textContent ////////////////////////
     colecRespuestas = colecNiveles[0].getColecRespuestas();
@@ -36,16 +39,19 @@ function extraerNivel() {
                                     <input type="radio" name="respuestas" id="respuesta0" value="${colecRespuestas[0]}">
                                     <label for="respuesta0"> ${colecRespuestas[0]}</label><br>
                                 </label>
+                                <br>
                                 <label class"anchisimo">
-                                    <input type="radio" name="respuestas" id="respuesta0" value="${colecRespuestas[1]}">
+                                    <input type="radio" name="respuestas" id="respuesta1" value="${colecRespuestas[1]}">
                                     <label for="respuesta1"> ${colecRespuestas[1]}</label><br>
                                 </label>
+                                <br>
                                 <label class"anchisimo">
-                                    <input type="radio" name="respuestas" id="respuesta0" value="${colecRespuestas[2]}">
+                                    <input type="radio" name="respuestas" id="respuesta2" value="${colecRespuestas[2]}">
                                     <label for="respuesta2"> ${colecRespuestas[2]}</label><br>
                                 </label>
+                                <br>
                                 <label class"anchisimo">
-                                    <input type="radio" name="respuestas" id="respuesta0" value="${colecRespuestas[3]}">
+                                    <input type="radio" name="respuestas" id="respuesta3" value="${colecRespuestas[3]}">
                                     <label for="respuesta3"> ${colecRespuestas[3]}</label><br>
                                 </label>
     `
